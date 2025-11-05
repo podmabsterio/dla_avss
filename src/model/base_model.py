a4f326d5629ce1a62b6ea2bbaf06d6362255c00d
markunya <mabondarenko223@gmail.com>
2025-11-06 01:32:09 +0300

    convtasnet done

---
+++ src/model/base_model.py
@@ -0,0 +1,15 @@
+from torch import nn
+
+class BaseModel(nn.Module):
+    def __init__(self):
+        super().__init__()
+
+    def __str__(self):
+        all_parameters = sum([p.numel() for p in self.parameters()])
+        trainable_parameters = sum(
+            [p.numel() for p in self.parameters() if p.requires_grad]
+        )
+        result_info = super().__str__()
+        result_info = result_info + f"\nAll parameters: {all_parameters}"
+        result_info = result_info + f"\nTrainable parameters: {trainable_parameters}"
+        return result_info
